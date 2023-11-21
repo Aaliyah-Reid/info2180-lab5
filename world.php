77a0f071d336d872f9f196c95462d7ffd5e90856
Aaliyah-Reid <aaliyahreid38@gmail.com>
2023-11-20 23:49:06 -0500

    handles request

--- world.php
+++ world.php
@@ -1,17 +1,40 @@
 <?php
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+if (isset($_GET['country'])){
+  $country = $_GET['country'];
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+  
 
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  if($results){
+    foreach ($results as $row){
+      echo "<h2>{$row['name']}</h2>";
+      echo "<p>Ruled by: {$row['head_of_state']}</p>";
+    }
+    
+  } else {
+    echo "Country not found.";
+  }
+
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+
+
+
+
+
